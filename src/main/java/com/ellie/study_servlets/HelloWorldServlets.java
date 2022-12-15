94bfd0b2ce28a517ebf1c0864b79ad51a56fb5aa
AnJiYeong <memarcis2@naver.com>
2022-12-15 10:58:05 +0900

    formsSelectsServlet

--- src/main/java/com/ellie/study_servlets/HelloWorldServlets.java
+++ src/main/java/com/ellie/study_servlets/HelloWorldServlets.java
@@ -8,11 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/helloWorldServlets with Message!")
+@WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServlets extends HttpServlet {
     protected void 	doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         
-        String message = "HelloWorldServlets !";
+        String message = "HelloWorldServlets with Message!";
+
         PrintWriter printWriter = response.getWriter();
         
         printWriter.println("<html lang='en'>");
